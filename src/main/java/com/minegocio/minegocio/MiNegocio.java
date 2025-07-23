e9cc928c8db918d0a0b3b6090f99058cd3b84419
POS <POS@POS>
2025-07-23 16:00:47 -0300

    *prueba de conexion con base.*prueba de creacion de articulo.

--- src/main/java/com/minegocio/minegocio/MiNegocio.java
+++ src/main/java/com/minegocio/minegocio/MiNegocio.java
@@ -4,6 +4,12 @@
 
 package com.minegocio.minegocio;
 
+import com.minegocio.model.Articulo;
+import com.minegocio.util.Conexion;
+import com.minegocio.DAO.ArticuloDAO;
+import com.minegocio.DaoImpl.ArticuloDAOImpl;
+import java.sql.Connection;
+
 /**
  *
  * @author POS
@@ -11,6 +17,41 @@ package com.minegocio.minegocio;
 public class MiNegocio {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //PRUEBA DE CONEXION
+    Connection con = Conexion.getConexion();
+    
+    if (con != null) {
+        System.out.println("Conexión exitosa!");
+    } else {
+        System.out.println("****Fallo la conexion.****");
     }
+
+       // Crear el objeto Articulo en base de datos mediante Procedimiento almacenado
+       
+        Articulo nuevoArticulo = new Articulo();
+        nuevoArticulo.setCodigo(203);
+        nuevoArticulo.setMarca(1);
+        nuevoArticulo.setCodDepartamento(2);
+        nuevoArticulo.setCodRubro(2);
+        nuevoArticulo.setCodFamilia(2);
+        nuevoArticulo.setDescripcion("Teclado mecánico ");
+        nuevoArticulo.setStock(100);
+        nuevoArticulo.setPrecioCosto(3500.0);
+        nuevoArticulo.setMargen(30.00);
+        nuevoArticulo.setPrecioVenta(4550.00);
+        nuevoArticulo.setEstado(1);
+        nuevoArticulo.setCodigoBarra(987654321);
+
+        // Usar el DAO para guardar el artículo
+        ArticuloDAO dao = new ArticuloDAOImpl();
+        boolean resultado = dao.crearArticulo(nuevoArticulo);
+
+        // Mostrar resultado
+        if (resultado) {
+            System.out.println(" Artículo creado correctamente.");
+        } else {
+            System.out.println(" Error al crear el artículo.");
+        }
+        }
+
 }
